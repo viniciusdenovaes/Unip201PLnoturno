3481d32cef08eaa4c72b562b9ff25fbf76d029d5
Vinicius de Novaes Guimaraes Pereira <viniciusdenovaes@gmail.com>
2020-02-28 21:45:56 -0300

    aula do dia 21/02/2020

---
+++ Aula01TiposPrimitivos/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int j = 0;
+		for(int i=0; i<args.length; j++) {
+			System.out.println("argumento " + (i++));
+			System.out.println("   " + args[j]);
+		}
+		System.out.println("terminou o programa");
+		
+		int a = 0;
+		int b = 1;
+		System.out.println("soma de " + a );
+		
+	}
+
+}
